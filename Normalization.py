a8f695c92599076aea922a522652eb7148b857fd
Jeeva261 <js3316573@gmail.com>
2024-09-16 18:13:42 +0530

    new changes

--- Normalization.py
+++ Normalization.py
@@ -1,9 +1,9 @@
-import pandas as pd
-from  sklearn.preprocessing import MinMaxScaler,StandardScaler
+import  pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-df=pd.read_json("file.json")
+file_path = 'C:/Users/ADMIN/OneDrive/Desktop/AIML_github/Normalization-and-standardization/file.json'
+df=pd.read_json(file_path)
 print(df)
 scaler=MinMaxScaler()
-
 new_scaler=scaler.fit_transform(df)
 print(new_scaler)
