2464170ea46a9b2d7865c67bc2722b3d2679ec02
Jeeva261 <js3316573@gmail.com>
2024-09-16 18:33:43 +0530

    modified:   Normalization.py         new file:   __init__.py         new file:   demo.py         new file:   file.csv

--- Normalization.py
+++ Normalization.py
@@ -1,9 +1,5 @@
 import  pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-file_path = 'C:/Users/ADMIN/OneDrive/Desktop/AIML_github/Normalization-and-standardization/file.json'
-df=pd.read_json(file_path)
+df=pd.read_csv("file.csv")
 print(df)
-scaler=MinMaxScaler()
-new_scaler=scaler.fit_transform(df)
-print(new_scaler)
